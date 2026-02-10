a4b311fc44de5a854b74f323f7c4b08d868b8b83
Aryan <142072964+Aerodia@users.noreply.github.com>
2026-02-10 12:25:43 +0530

    Time: 1 ms (96.22%) | Memory: 47 MB (40.13%) - LeetSync

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.java
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.java
@@ -2,10 +2,8 @@ class Solution {
     public int earliestTime(int[][] tasks) {
         int min = Integer.MAX_VALUE;
         for(int i = 0 ; i<tasks.length;i++){
-            for(int j = 0 ; j<2;j++){
                 int add = tasks[i][0] + tasks[i][1];
                 min = Math.min(min,add);
-            }
         }
         return min;
     }
