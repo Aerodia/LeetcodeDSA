7097be38c48ecc91b03dabc7324710f8dc630ed1
Aryan <142072964+Aerodia@users.noreply.github.com>
2026-05-12 12:40:58 +0530

    Time: 29 ms (23.08%) | Memory: 114.1 MB (19.23%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) ->
+            (b[1] - b[0]) - (a[1] - a[0])
+        );
+
+        int l = 0;
+        int r = (int)1e9;
+        int ans = Integer.MAX_VALUE;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (isPossible(tasks, mid)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+    private boolean isPossible(int[][] tasks, int energy) {
+        int current = energy;
+        for (int[] task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+            if (current < minimum) {
+                return false;
+            }
+            current -= actual;
+        }
+
+        return true;
+    }
+}
