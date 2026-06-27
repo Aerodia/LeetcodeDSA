cab1436782a21d74b75454f2bccae4b790480eca
Aryan <142072964+Aerodia@users.noreply.github.com>
2026-06-27 14:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (14.31%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int primary = 0;
-        int secondary = 0;
         int n = mat.length;
+        int primary = 0 ;
+        int secondary = 0 ;
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i <n; i++){
             primary += mat[i][i];
-            secondary += mat[i][n - 1 - i];
+            secondary += mat[i][n - 1 -i];
         }
 
-        if (n % 2 == 1) {
-            primary -= mat[n / 2][n / 2];
+        if(n%2 == 1){
+            primary -= mat[n/2][n/2];
         }
         return primary + secondary;
     }
