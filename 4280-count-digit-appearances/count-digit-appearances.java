71c566bc4c9a3b0e2fe4ce0d3753b66164ed0fb5
Aryan <142072964+Aerodia@users.noreply.github.com>
2026-07-03 20:49:44 +0530

    Time: 3 ms (100.00%) | Memory: 47.1 MB (36.30%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.java
+++ 4280-count-digit-appearances/count-digit-appearances.java
@@ -1,16 +1,13 @@
 class Solution {
     public int countDigitOccurrences(int[] nums, int digit) {
-        int count = 0 ;
-        for(int i = 0 ; i<nums.length;i++){
-            int num = nums[i];
-            while(num>0){
-                int digitinnum = num%10;
-                if(digitinnum == digit){
-                    count++;
-                }
+        int[] arr = new int[10];
+
+        for(int num : nums){
+            while(num > 0){
+                arr[num%10]++;
                 num/=10;
             }
         }
-        return count;
+        return arr[digit];
     }
 }
